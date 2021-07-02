6a888548c212a52a5c5129b9881796c4e173a908
NataliaSova <rot.fuchslein@gmail.com>
2021-07-02 21:18:14 +0500

    Simple Counter JS

---
+++ simpleCounter JS/app.js
@@ -0,0 +1,34 @@
+let count = 0;
+
+const numberValue = document.getElementById('number-value');
+
+const btns = document.querySelectorAll('.btn');
+
+
+if(btns) {
+    btns.forEach(function(button) {
+        button.addEventListener('click', pressedBtn => {
+
+            const styles = pressedBtn.currentTarget.classList;
+
+            if(styles.contains('decrease')) {
+                count--;
+            } else if(styles.contains('increase')) {
+                count++;
+            } else {
+                count = 0;
+            }
+            
+            if (count == 0) {
+                numberValue.style.color = 'black';
+            } else if (count > 0) {
+                numberValue.style.color = 'green';
+            } else {
+                numberValue.style.color = 'red';
+            }  
+            
+            numberValue.textContent = count;
+        })
+    });
+}
+
