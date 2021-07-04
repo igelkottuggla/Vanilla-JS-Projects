f6131677e98715329eaebe6d7f061eb255da3cc3
NataliaSova <rot.fuchslein@gmail.com>
2021-07-04 18:57:43 +0500

    Sidebar JS, Modal JS with some changes to css as well

---
+++ Sidebar JS/app.js
@@ -0,0 +1,18 @@
+const headerContent = document.querySelector('.content-wrapper');
+
+if(headerContent) {
+    const toggleBtn = headerContent.querySelector('.sidebar-toggle');
+    const sidebar = headerContent.querySelector('.sidebar');
+    const closeBtn = headerContent.querySelector('.close-btn');
+    
+    toggleBtn.addEventListener('click', function() {
+        sidebar.classList.toggle('show-sidebar');
+    });
+    
+    closeBtn.addEventListener('click', function() {
+        sidebar.classList.remove('show-sidebar');
+    });
+}
+
+
+
