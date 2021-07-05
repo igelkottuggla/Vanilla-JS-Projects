0ca1c20360fcac65b8e531e5fb2748056d83227f
NataliaSova <rot.fuchslein@gmail.com>
2021-07-05 17:31:59 +0500

    Menu JS, Video JS

---
+++ Video JS/app.js
@@ -0,0 +1,20 @@
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function() {
+    preloader.classList.add('hide-preloader');
+});
+
+const controlBtn  = document.querySelector('.switch-btn');
+const video  = document.querySelector('.video-container');
+
+if (controlBtn) {
+    controlBtn.addEventListener('click', function() {
+        if(!controlBtn.classList.contains('slide')) {
+            controlBtn.classList.add('slide');
+            video.pause();
+        } else {
+            controlBtn.classList.remove('slide');
+            video.play();
+        }
+    });
+}
