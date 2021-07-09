50401cc3c1faa9c240e02490ad4d357c6498d3bc
NataliaSova <rot.fuchslein@gmail.com>
2021-07-09 18:53:45 +0500

    Slider JS with some corrections - chahged the course initial logic of working with slider buttons

---
+++ Slider JS/app.js
@@ -0,0 +1,59 @@
+const slider = document.querySelector('.slider');
+
+if(slider) {
+    const slides = slider.querySelectorAll('.slide');
+    const nextBtn = slider.querySelector('.nextBtn');
+    const prevBtn = slider.querySelector('.prevBtn');
+
+    let counter = 0;
+
+    slides.forEach((slide, index) => {
+        slide.style.left = `${index * 100}%`;
+    });        
+
+    const slidesChanging = () => {           
+        prevBtn.classList.toggle('btn-hidden', counter  === 0);
+        nextBtn.classList.toggle('btn-hidden', counter === --slides.length);
+
+        slides.forEach(slide => {
+            slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+
+        // 1st option **** infinite slides' changing **** 
+            // if(counter === slides.length) {
+            //     counter = 0;
+            // }
+
+            // if(counter < 0) {
+            //     counter = slides.length -1;
+            // }
+
+        // 2nd option from the original course 
+            // if (counter < slides.length - 1) {
+            //     nextBtn.style.display = "block";
+            //   } else {
+            //     nextBtn.style.display = "none";
+            //   }
+            //   if (counter > 0) {
+            //     prevBtn.style.display = "block";
+            //   } else {
+            //     prevBtn.style.display = "none";
+            //   }
+
+        // 3nd option  **** when the slides end buttons are blocked**** 
+    }   
+
+    // prevBtn.style.display = "none";  original version from the course
+
+
+    nextBtn.addEventListener('click', function() {
+        counter++;
+        slidesChanging();
+    });
+
+    prevBtn.addEventListener('click', function() {
+        counter--;
+        slidesChanging();
+    });
+}
+
